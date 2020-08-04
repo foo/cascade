8b8abcb34dd49fa0970d3a0d104817489f08283e
Judafa <62807929+Judafa@users.noreply.github.com>
2020-08-04 11:25:45 +0200

    - added a class that manage the examples files (max cost phases, some counter example if there will be) - launching solve_phase.py now check whether the property costs <= l * s * log(s) is true or not, in addition to searching the max cost phase

---
+++ src/Min_Cascade_Algo/file_manager.py
@@ -0,0 +1,46 @@
+import sys
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+import os.path
+from pathlib import Path
+
+class file_management:
+
+    def __init__(self, output_file):
+        self.output_file = output_file
+        self.my_file = None
+
+    def get_or_create_file(self, k, l, choose_mode):
+        if not os.path.exists(self.output_file + "/" + str(choose_mode)):
+            Path(self.output_file + "/" + str(choose_mode)).mkdir(parents=True, exist_ok=True)
+        self.output_file += "/" + str(choose_mode)
+        if not os.path.exists(self.output_file + "/" + choose_mode + " l=" + str(l)):
+            Path(self.output_file + "/" + choose_mode + " l=" + str(l)).mkdir(parents=True, exist_ok=True)
+        self.output_file += "/" + choose_mode + " l=" + str(l)
+        if len([filename for filename in os.listdir(self.output_file) if
+                filename.startswith(choose_mode + " l=" + str(l) + " k=" + str(k))]) == 0:
+            self.output_file += "/" + choose_mode + " l=" + str(l) + " k=" + str(k) + " bs=0.txt"
+            self.my_file = open(self.output_file, "w+")
+            best_score = 0
+        else:
+            file_name = [filename for filename in os.listdir(self.output_file) if
+                         filename.startswith(choose_mode + " l=" + str(l) + " k=" + str(k))][0]
+            best_score = int(file_name.split(" ")[-1].split(".")[0][3:])
+            self.output_file += "/" + file_name
+            self.my_file = open(self.output_file, "w+")
+        return best_score
+
+    def update_file(self, ):
+
+
+
+    def rename_best_score_file(self, best_score, output_file):
+        split = output_file.split(" ")
+        del split[-1]
+        split.append("bs=" + str(best_score) + ".txt")
+        tmp = ""
+        for s in split:
+            tmp += s + " "
+        tmp = tmp[:len(tmp) - 1]
+        return tmp
